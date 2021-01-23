e5f63dda4ee92a0c30e9ea5d24341670587877dd
ogbe <ogbejude24@gmail.com>
2021-01-24 00:46:59 +0100

    ft adds react-babel

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<h1>Hello</h1>, rootElement)
